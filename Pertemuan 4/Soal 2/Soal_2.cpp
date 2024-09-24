254a839906178d22026f7ed87a123fd4a76f5441
farsyaant <farsyanabilatori@mail.ugm.ac.id>
2024-09-24 14:52:58 +0700

    Create Soal_2.cpp

---
+++ Pertemuan 4/Soal 2/Soal_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void multiplicationTable(int n) {
+    for (int i = 1; i <= 10; i++) {
+        cout << n << " x " << i << " = " << n * i << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter an integer: ";
+    cin >> n;
+    multiplicationTable(n);
+    return 0;
+}
