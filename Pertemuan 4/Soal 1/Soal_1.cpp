c2b0488c7d0e8497302e4d240e8a714919a9deb8
farsyaant <farsyanabilatori@mail.ugm.ac.id>
2024-09-24 17:04:29 +0700

    Update Soal_1.cpp

--- Pertemuan 4/Soal 1/Soal_1.cpp
+++ Pertemuan 4/Soal 1/Soal_1.cpp
@@ -1,17 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
-int main()
-{
+
+string Soal_1(int n) {
+	string result;
 	int n;
-	cout << "Input: ";
-	cin >> n;
 	
-	int jumlah = 0;
 	for (int i=1; i <= n; ++i ){
-		jumlah += i ;
+		result += " " ;
 	}
 
-	cout << "Output: " << jumlah;
+	return result;
+}
 
-	return 0;
+
+int main()
+{	ifstream testFile("Test.txt);
+ 	int input;
+    	string expected_output;
+
+    if (!testFile) {
+        std::cerr << "Error: Could not open test.txt" << std::endl;
+        return 1;
+    }
+
+    int test_num = 1;
+    while (testFile >> input && std::getline(testFile >> std::ws, expected_output)) {
+        std::string output = Soal_1(input);
+        if (output == expected_output) {
+            std::cout << "Test " << test_num << " passed!" << std::endl;
+        } else {
+            std::cout << "Test " << test_num << " failed. Expected: \"" 
+                      << expected_output << "\", Got: \"" << output << "\"" << std::endl;
+        }
+        test_num++;
+    }
+
+    testFile.close();
+    return 0;
 }
