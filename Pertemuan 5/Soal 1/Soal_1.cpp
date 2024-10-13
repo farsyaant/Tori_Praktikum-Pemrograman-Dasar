53ad1951559d5d7a0981de1b1e85f6a3a7ce4ced
farsyaant <farsyanabilatori@mail.ugm.ac.id>
2024-10-13 21:57:07 +0700

    Update Soal_1.cpp

--- Pertemuan 5/Soal 1/Soal_1.cpp
+++ Pertemuan 5/Soal 1/Soal_1.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
+int main() {
+    int n = 100;
+    vector<string> result(n);
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            result[i-1] = "FizzBuzz";
+        }
+        else if (i % 3 == 0) {
+            result[i-1] = "Fizz";
+        }
+        else if (i % 5 == 0) {
+            result[i-1] = "Buzz";
+        }
+        else {
+            result[i-1] = to_string(i);
+        }
+    }
+    
+    for (const string& output: result) {
+    cout << output << " ";
+    }
+}
