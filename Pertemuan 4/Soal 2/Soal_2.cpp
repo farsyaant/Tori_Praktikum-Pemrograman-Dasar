dc41bf2aa1efe6ab239433d0da80f15f6c870d98
farsyaant <farsyanabilatori@mail.ugm.ac.id>
2024-10-14 22:27:02 +0700

    Update Soal_2.cpp

--- Pertemuan 4/Soal 2/Soal_2.cpp
+++ Pertemuan 4/Soal 2/Soal_2.cpp
@@ -4,14 +4,12 @@
 using namespace std;
 
 string Soal_2(int n) {
-    string result; // Create a string to hold the multiplication table
-
-    // Generate the multiplication table
-    for (int i = 1; i <= 10; i++) {
-        result += to_string(n) + " * " + to_string(i) + " = " + to_string(n * i) + "\n"; // Append each line to result
-    }
+    string result; 
     
-    return result; // Return the entire multiplication table as a string
+    for (int i = 1; i <= 10; i++) {
+        result += to_string(n) + " * " + to_string(i) + " = " + to_string(n * i) + "\n"; 
+    }
+    return result; 
 }
 
 int main() {
@@ -27,16 +25,15 @@ int main() {
     int test_num = 1;
     while (testFile >> input) {
         testFile.ignore(); // Ignore the newline after input
-        // Read the expected output until the next input (or end of file)
+       
         string expected_output = "";
         string line;
         while (getline(testFile, line) && !line.empty()) {
             expected_output += line + "\n";
         }
 
-        string output = Soal_2(input); // Get the multiplication table as a string
+        string output = Soal_2(input); 
 
-        // Trim the output and expected_output strings for comparison
         if (output == expected_output) {
             cout << "Test " << test_num << " passed!" << endl;
         } else {
