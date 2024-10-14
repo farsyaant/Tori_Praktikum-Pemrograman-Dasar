2e45dc103d895a4d4b03eb3af3008e40386c1363
farsyaant <farsyanabilatori@mail.ugm.ac.id>
2024-10-15 00:13:19 +0700

    Update Soal_1.cpp

--- Pertemuan 5/Soal 1/Soal_1.cpp
+++ Pertemuan 5/Soal 1/Soal_1.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 using namespace std;
 
-int main() {
-    int n = 100;
+string Soal_1(int n) {
     vector<string> result(n);
     for (int i = 1; i <= n; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
@@ -21,8 +21,35 @@ int main() {
         }
     }
     
-    for (const string& output: result) {
-    cout << output << " ";
+    string output;
+    for (const string& s: result) {
+        output += s + " ";
     }
-    return 0;
+    return output;
 }
+
+    int main() {
+        ifstream testFile("Test.txt");
+ 	int input;
+    	string expected_output;
+
+    if (!testFile) {
+        cerr << "Error: Could not open test.txt" << endl;
+        return 1;
+    }
+
+    int test_num = 1;
+    while (testFile >> input && std::getline(testFile >> ws, expected_output)) {
+        string output = Soal_1(input);
+        if (output == expected_output) {
+            cout << "Test " << test_num << " passed!" << endl;
+        } else {
+            std::cout << "Test " << test_num << " failed. Expected: \"" 
+                      << expected_output << "\", Got: \"" << output << "\"" << endl;
+        }
+        test_num++;
+    }
+
+    testFile.close();
+    return 0;
+    }	
