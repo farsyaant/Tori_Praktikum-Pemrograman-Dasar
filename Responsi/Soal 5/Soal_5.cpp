6562983eeb00b24bbe191abe52120c2cdfed6f83
farsyaant <farsyanabilatori@mail.ugm.ac.id>
2024-11-26 14:37:29 +0700

    Update Soal_5.cpp

--- Responsi/Soal 5/Soal_5.cpp
+++ Responsi/Soal 5/Soal_5.cpp
@@ -3,23 +3,20 @@
 using namespace std;
 
 int main() {
-    int num, digit, rev = 0;
+    int digit, rev = 0;
     
-    int n = 12321;
-    string cek = "Bilangan palindrom.";
+    int num = 12321;
+    string hasil = "Bilangan palindrom.";
     
-    n = num;
     while (num != 0) {
         digit = num % 10;
         rev = rev * 10 + digit;
         num = num / 10;
     }
     
+    bool cek = false;
     if (rev == num) {
-        bool rev = true;
-    }
-    else {
-        bool rev = false;
+        cek = true;
     }
     
     if (rev) {
